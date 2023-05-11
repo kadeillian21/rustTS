c87f897609937621348101a2dcf0c95b52967c7d
Kade Illian <kade.illian@biproxi.com>
2023-05-10 19:23:59 -0600

    read dim files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import fs from "fs"
+
 const list = [1, 2, 3]
 const addOneList = list.map(number => number +1)
 console.log(addOneList)
+
+fs.readFileSync("lines").toString().split("\n").forEach(line => console.log(line))
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
 fn main() {
-    let data = vec![1, 2, 3];
-    let mut foo = data.iter().map(|x| x + 1);
-    let mut new_vector = vec![];
-    while let Some(x) = foo.next() {
-        new_vector.push(x)
-    }
-    println!("{:?}", new_vector)
+    let file = std::fs::read_to_string("lines").unwrap();
+    file.lines().for_each(|line| println!("{}", line));
 }
